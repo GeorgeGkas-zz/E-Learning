981028b8748342ea9374b9b7efc28f7992370e86
GeorgeGkas <georgegkas@gmail.com>
2015-01-29 16:37:06 +0200

    Update count.cpp

--- Linked_Lists/count.cpp
+++ Linked_Lists/count.cpp
@@ -2,49 +2,46 @@
 #include <cstdio>
 
 struct node {
-	int value;
+    int value;
     node* next;
 	
 };
 
 node* create_node() {
-	node* ret = new node;
-	ret->next = NULL;
+    node* ret = new node;
+    ret->next = NULL;
 } 
 
 
 int count(struct node* head, int frequency) {
 	
-	struct node* current = head;
-
+    struct node* current = head;
     int count = 0;
 
     while (current != NULL) {
-        if (current->value == frequency)
-        count++;
-	 
+        if (current->value == frequency) {
+            count++;
+        }
         current = current->next;
-
     }
 
     return count;
-
 }
 
 void insert(struct node** root,struct node** prev, struct node** current, int element) {
 	
-	
-	(*current) = create_node();
+    (*current) = create_node();
+    (*current)->value = element;
 		
-		(*current)->value = element;
-		
-		if ((*root) == NULL) 
-			(*root) = (*current); 
-		else
-			(*prev)->next = (*current); 
-		
-		(*prev) = (*current);
-	
+    if((*root) == NULL) {
+    (*root) = (*current); 
+    }
+	 
+    else{
+    (*prev)->next = (*current); 
+    }
+    
+    (*prev) = (*current);
 }
 
 int main() {
@@ -63,24 +60,19 @@ int main() {
 	
 	
 	for (int i = 0; i < n; ++i) {
-		int element;
-		scanf("%d", &element);
-		
-		current = create_node();
-		
-		insert(&root,&prev,&current,element);
-		
+            int element;
+            scanf("%d", &element);		
+            insert(&root,&prev,&current,element);
 	}
 	
 	
-	/* printf("\nNow printing elements:\n");
+	printf("\nNow printing elements:\n");
 	current = root;
+	
 	while (current != NULL) {
-		printf("%i\n", current->value);
-		current = current->next;
-	} */
-	
-	
+            printf("%i\n", current->value);
+            current = current->next;
+	} 
 	
 	printf("count of %d is %d",frequency, count(root, frequency)); 
 	
