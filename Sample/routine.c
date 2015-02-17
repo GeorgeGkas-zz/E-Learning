5cc596371cac2917939e45c763dad479b8ed1000
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-02-17 22:03:15 +0200

    Update routine.c

--- Sample/routine.c
+++ Sample/routine.c
@@ -7,7 +7,7 @@
 //is the following work as the above?
 
 if (ProfitA == ProfitC ) {
-            if (ProfitB = ProfitA) {
+            if (ProfitB == ProfitA) {
             bestMin = ProfitA;
             printf("\n\tThe optimal best solution is %d\n", bestMin);
             return 0;
