c31aec59fe27c3170b913927278ab49313d1f7ab
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-02-17 21:39:25 +0200

    Create routine.c

---
+++ Sample/routine.c
@@ -0,0 +1,5 @@
+ if (ProfitA == ProfitB == ProfitC) {
+            bestMin = ProfitA; //the person don't matter
+            printf("\n\tThe optimal best solution is %d\n", bestMin);
+            return 0;
+        }
