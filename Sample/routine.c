e130723469582d28c1ca7ff5e2c8ecb18f7f987f
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-02-18 14:18:36 +0200

    Update routine.c

--- Sample/routine.c
+++ Sample/routine.c
@@ -1,11 +1,3 @@
- if (ProfitA == ProfitB == ProfitC) {
-            bestMin = ProfitA; //the person don't matter
-            printf("\n\tThe optimal best solution is %d\n", bestMin);
-            return 0;
-        }
-
-//is the following work as the above?
-
 if (ProfitA == ProfitC ) {
             if (ProfitB == ProfitA) {
             bestMin = ProfitA;
