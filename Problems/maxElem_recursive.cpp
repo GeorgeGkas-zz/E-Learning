8f212d41f5ee780b342a028b5f6ac62a3fe66a99
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-03-22 16:36:01 +0200

    Update maxElem_recursive.cpp

--- Problems/maxElem_recursive.cpp
+++ Problems/maxElem_recursive.cpp
@@ -3,20 +3,20 @@
 #include <iostream>
 using namespace std;
 
-int maxelem(int N,int i, int best) {
+int maxelem(int N, int i, int best) {
 	if (N == 0) return best; // check if we run out of numbers to enter
 
 	cin >> i; // pass the element in current state
-	if ( i > best) { // check for max
+	if (i > best) { // check for max
 	best = i;
 	}
 
-	maxelem(--N,i, best); // decrease the N by one.
+	maxelem(--N, i, best); // decrease the N by one.
 }
 
 int main() {
 	int N = 0; // assume that we know the number of elements
 	cin >> N;
-	cout << "Max number: " << maxelem(N,0, 0) << endl;
+	cout << "Max number: " << maxelem(N, 0, 0) << endl;
 	return 0;
 }
