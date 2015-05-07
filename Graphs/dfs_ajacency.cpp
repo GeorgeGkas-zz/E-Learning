480ba2bbbe2c3f00f60114f56b556aee31899617
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-05-07 18:28:33 +0300

    Update dfs_ajacency.cpp

--- Graphs/dfs_ajacency.cpp
+++ Graphs/dfs_ajacency.cpp
@@ -12,3 +12,32 @@
          }
       }
    }
+   
+   //DFS:
+//ok now the quetions:
+//  I named an adjacency list. Can i made it to matrix as well?
+//  if not then I need to know more about adjacency lists!
+void dfs(int start)
+{
+    int j,temp; 
+    stack<int> s; //STACK
+    s.push(start);
+    vector<int>:: iterator it;
+    while(s.empty()==0)
+    {
+        temp=s.top(); // Top element from Stack
+        s.pop();
+        status[temp]=1; // marked as visited , 0 means unvisited
+        cout<<temp<<endl; // visited element
+        for(it=list[temp].begin();it!=list[temp].end();it++)
+        {
+            j=*it;
+            if(status[j]==0)
+            {
+                s.push(j);
+                status[j]=2;    // means that it is in stack        
+            }
+        }
+
+    }
+}
