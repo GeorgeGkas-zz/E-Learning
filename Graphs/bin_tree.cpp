3a7da4923f0f8eeb8fc48ac666944a921f182a2c
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-05-29 16:17:05 +0300

    Update bin_tree.cpp

--- Graphs/bin_tree.cpp
+++ Graphs/bin_tree.cpp
@@ -4,68 +4,68 @@ using namespace std;
 
 //Begin the construction of the BINARY TREE
 struct tree_node {
-	tree_node *left;
-	tree_node *right;
-	int data;
+  tree_node *left;
+  tree_node *right;
+  int data;
 };
 
 //Declaring the class
 class bst {
-	tree_node *root; //creating the root of the binary tree
-	
-	public:
-		bst() {
-			root = NULL; //intialize the default construction, set the root to NULL
-		}
-		
-		int is_empty() { //check for empty graph
-			return (root == NULL);
-		}
-		
-		//Manipulating the Binary Tree
-		void insert(int item);
-		void remove(int value); //difficult implementation
-		
-		//Graph Traversal of Binary Tree
-		void in_order_trav();
-		void in_order(tree_node *);
-		void pre_order_trav();
-		void pre_order(tree_node *);
-		void post_order_trav();
-		void post_order(tree_node *);
+  tree_node *root; //creating the root of the binary tree
+  
+  public:
+    bst() {
+      root = NULL; //intialize the default construction, set the root to NULL
+    }
+    
+    int is_empty() { //check for empty graph
+      return (root == NULL);
+    }
+    
+    //Manipulating the Binary Tree
+    void insert(int item);
+    void remove(int value); //difficult implementation
+    
+    //Graph Traversal of Binary Tree
+    void in_order_trav();
+    void in_order(tree_node *);
+    void pre_order_trav();
+    void pre_order(tree_node *);
+    void post_order_trav();
+    void post_order(tree_node *);
 };
 
 /*************Insert Procedure*****************************/
 // Begin
 void bst::insert(int item) {
-	tree_node *p = new tree_node;
-	tree_node *parent;
-	p->data = item;
-	p->left = NULL;
-	p->right = NULL;
-	parent = NULL;
+  tree_node *p = new tree_node;
+  tree_node *parent;
+  p->data = item;
+  p->left = NULL;
+  p->right = NULL;
+  parent = NULL;
 
-	if (is_empty()) {
-		root = p;
-	}
-	else {
-		tree_node *ptr;
-		ptr = root;
+  if (is_empty()) {
+    root = p;
+  }
+  else {
+    tree_node *ptr;
+    ptr = root;
 
-		while (ptr != NULL) {
-			parent = ptr;
+    while (ptr != NULL) {
+      parent = ptr;
 
-			if (item > ptr->data)
-				ptr = ptr->right;
-			else
-				ptr = ptr->left;
-		}
+      if (item > ptr->data)
+        ptr = ptr->right;
+      else
+        ptr = ptr->left;
+    }
 
-		if (item < parent->data) 
-			parent->left = p;
-		else
-			parent->right = p;
-	}
+    if (item < parent->data) 
+      parent->left = p;
+    else
+      parent->right = p;
+  }
 }
 // End
 /***********************************************************/
@@ -73,46 +73,46 @@ void bst::insert(int item) {
 /*************Remove Procedure*****************************/
 // Begin
 void bst::remove(int item) {
-	//lets try to find if the tree isn't existing
-	if (is_empty()) {
+  //lets try to find if the tree isn't existing
+  if (is_empty()) {
     cout<<" This Tree is empty! "<<endl;
     return;
-	}
+  }
 
-	//now we look if the value we pass is exist in the tree
-	bool found = false; //exist or not?
-	tree_node* curr;
-	tree_node* parent;
-	curr = root;
+  //now we look if the value we pass is exist in the tree
+  bool found = false; //exist or not?
+  tree_node* curr;
+  tree_node* parent;
+  curr = root;
 
-	while (curr != NULL) {
+  while (curr != NULL) {
     if (curr->data == item) {
-			found = true;
-			cout << "element found and removed" << endl;
-			break;
-		}
-		else {
-			parent = curr;
-			if(item > curr->data) curr = curr->right;
-			else curr = curr->left;
-		}
-	}
-	if (!found) {
-		cout<<" Data not found! "<<endl;
-		return;
-	}
+      found = true;
+      cout << "element found and removed" << endl;
+      break;
+    }
+    else {
+      parent = curr;
+      if(item > curr->data) curr = curr->right;
+      else curr = curr->left;
+    }
+  }
+  if (!found) {
+    cout<<" Data not found! "<<endl;
+    return;
+  }
 
-	// 3 cases :
+  // 3 cases :
   // 1. We're removing a leaf node
   // 2. We're removing a node with a single child
   // 3. we're removing a node with 2 children
 
-	// Node with single child
+  // Node with single child
   if ((curr->left == NULL && curr->right != NULL) || (curr->left != NULL && curr->right == NULL)) {
     if (curr->left == NULL && curr->right != NULL) {
-    	if (parent->left == curr) {
-      	parent->left = curr->right;
-      	delete curr;
+      if (parent->left == curr) {
+        parent->left = curr->right;
+        delete curr;
       }
       else {
         parent->right = curr->left;
@@ -179,15 +179,15 @@ void bst::remove(int item) {
 /*************In Order Traversal*****************************/
 // Begin
 void bst::in_order_trav() {
-	in_order(root);
+  in_order(root);
 }
 
 void bst::in_order(tree_node *ptr) {
-	if (ptr!=NULL) {
-		in_order(ptr->left);
-		cout << " " << ptr->data << "     ";
-		in_order(ptr->right);
-	}
+  if (ptr!=NULL) {
+    in_order(ptr->left);
+    cout << " " << ptr->data << "     ";
+    in_order(ptr->right);
+  }
 }
 // End
 /***********************************************************/
@@ -195,15 +195,15 @@ void bst::in_order(tree_node *ptr) {
 /*************Pre Order Traversal*****************************/
 // Begin
 void bst::pre_order_trav() {
-	pre_order(root);
+  pre_order(root);
 }
 
 void bst::pre_order(tree_node *ptr) {
-	if (ptr!=NULL) {
-		cout << "  " << ptr->data << "     ";
-		pre_order(ptr->left);
-		pre_order(ptr->right);
-	}
+  if (ptr!=NULL) {
+    cout << "  " << ptr->data << "     ";
+    pre_order(ptr->left);
+    pre_order(ptr->right);
+  }
 }
 // End
 /***********************************************************/
@@ -211,33 +211,33 @@ void bst::pre_order(tree_node *ptr) {
 /*************Post Order Traversal*****************************/
 // Begin
 void bst::post_order_trav() {
-	post_order(root);
+  post_order(root);
 }
 
 void bst::post_order(tree_node *ptr) {
-	if(ptr!=NULL) {
-		post_order(ptr->left);
-		post_order(ptr->right);
-		cout << "  " << ptr->data << "     ";
-	}
+  if(ptr!=NULL) {
+    post_order(ptr->left);
+    post_order(ptr->right);
+    cout << "  " << ptr->data << "     ";
+  }
 }
 // End
 /***********************************************************/
 
 int main() {
-	bst b;
-	b.insert(52);
-	b.insert(25);
-	b.insert(50);
-	b.insert(15);
-	b.insert(40);
-	b.insert(45);
-	b.insert(20); cout<<"inorder"<<endl;
-	b.in_order_trav();
-	cout<<endl<<"postorder"<<endl;
-	b.post_order_trav();
-	cout<<endl<<"preorder"<<endl;
-	b.pre_order_trav();
-	cout << "remove" << endl;
-	b.remove(15);
+  bst b;
+  b.insert(52);
+  b.insert(25);
+  b.insert(50);
+  b.insert(15);
+  b.insert(40);
+  b.insert(45);
+  b.insert(20); cout<<"inorder"<<endl;
+  b.in_order_trav();
+  cout<<endl<<"postorder"<<endl;
+  b.post_order_trav();
+  cout<<endl<<"preorder"<<endl;
+  b.pre_order_trav();
+  cout << "remove" << endl;
+  b.remove(15);
 }
