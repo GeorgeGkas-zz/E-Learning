053a1bfa17b10d7f270a531b6eb59ef961c8a8b9
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-09-03 21:01:54 +0300

    Update brackets.py

--- Problems/brackets.py
+++ Problems/brackets.py
@@ -1,30 +1,25 @@
 # Read the brackets that have form open-close relationship.
-isOpen = "("
-isClose = ")"
+def get_bracket():
+    isOpen = "("
+    isClose = ")"
+    open_brackets = list()
 
-open_brackets = list()
+    str_message = raw_input("Please the required text:\n")
+    lst_symbols = list() # Get the required symbols in a list
 
-str_message = raw_input("Please the required text:\n")
-lst_symbols = list() # Get the required symbols in a list
-
-if str_message[0] is isClose:
-	print "Close vracket in the beginning found"
-elif str_message[0] is isOpen:
-	open_brackets.append(str_message[0])
-
-for i in str_message[1:]:
-		if i is isOpen:
-			open_brackets.append(i)
-		
-		elif i is isClose:
-			if len(open_brackets) == 0:
-				print "No open brackets found "
-				exit(1)
-			else:
-				open_brackets.pop()
-			
-if open_brackets:
-	print "You have forgot to close brackets"
-else:
-	print 'Programm terminated successfuly'
-
+    for i in str_message:
+        if i is isOpen:
+            open_brackets.append(i)
+            
+        elif i is isClose:
+            if len(open_brackets) == 0:
+                return False
+            else:
+                open_brackets.pop()
+            
+    if open_brackets:
+        return False
+    else:
+        return True
+    
+is_bracket_correct = get_bracket()
