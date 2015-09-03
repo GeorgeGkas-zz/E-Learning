2ac21103ec916bf313f1b33b174a4cf28bbd9dee
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-09-03 19:50:21 +0300

    Update brackets.py

--- Problems/brackets.py
+++ Problems/brackets.py
@@ -1,49 +1,30 @@
 # Read the brackets that have form open-close relationship.
+isOpen = "("
+isClose = ")"
 
-symbols_to_read = "()"
-
-def get_symbols(symbols_str):
-	str_message = raw_input("Please the required text:\n")
-	lst_symbols = list() # Get the required symbols in a list
-	for i in str_message:
-		if i in symbols_str:
-			lst_symbols.append(i)
-	return lst_symbols
-
-
-
-lst_symbols = get_symbols(symbols_to_read)
-
-if not lst_symbols:
-	print "No given symbols found in your text"
-	exit(1)
-
-if lst_symbols[0] is ")":
-	print "Warning: Close bracket in start of the text is not accepted"
-	exit(1)
-
-# no Stack structure in Python but we can use lists
 open_brackets = list()
-close_brackets = list()
 
-for i in lst_symbols:
-	if i is "(":
-		open_brackets.append(i)
-	else:
-		close_brackets.append(i)
+str_message = raw_input("Please the required text:\n")
+lst_symbols = list() # Get the required symbols in a list
 
-print open_brackets
-print close_brackets
+if str_message[0] is isClose:
+	print "Close vracket in the beginning found"
+elif str_message[0] is isOpen:
+	open_brackets.append(str_message[0])
 
-for i in range(0, len(open_brackets)):
-	if open_brackets and close_brackets:
-		open_brackets.pop()
-		close_brackets.pop()
-		i += 1
+for i in str_message[1:]:
+		if i is isOpen:
+			open_brackets.append(i)
+		
+		elif i is isClose:
+			if len(open_brackets) == 0:
+				print "No open brackets found "
+				exit(1)
+			else:
+				open_brackets.pop()
+			
+if open_brackets:
+	print "You have forgot to close brackets"
+else:
+	print 'Programm terminated successfuly'
 
-
-if open_brackets or close_brackets:
-	if open_brackets > close_brackets:
-		print "You forgot close brackets"
-	else:
-		print "You forgot open brackets"
