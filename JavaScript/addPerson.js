870cc120744610f30827e576a95e055f64bf393a
GeorgeGkas <GeorgeGkas@users.noreply.github.com>
2015-12-23 23:08:16 +0200

    Update addPerson.js

--- JavaScript/addPerson.js
+++ JavaScript/addPerson.js
@@ -1,21 +1,28 @@
+var add_xmlHttp = new XMLHttpRequest();
+
 function Add_handleServerResponse() {
     if (add_xmlHttp.readyState == 4 ) {
         if (add_xmlHttp.status == 200) {
-           alert('worked!');
+            xmlResponse = add_xmlHttp.responseXML;
+            xmlDocumentElement = xmlResponse.documentElement;
+            var result = JSON.parse(xmlDocumentElement.firstChild.data);
+            alert(result['msg']);
         }
         else {
-           alert('It didn\'t work ' + add_xmlHttp.status + ' ' + add_xmlHttp.readyState);
+           alert('Could not pass data to js. Status:' + add_xmlHttp.status + ' Ready state:' + add_xmlHttp.readyState);
 
         }
     }
 }
 
-function AddPerson() {
-        if ((add_xmlHttp.readyState == 0 || add_xmlHttp.readyState == 4) && document.getElementById("PersonSearchInput").value != "") {
-        person = encodeURIComponent(document.getElementById("PersonSearchInput").value);
+function AddPerson(person) {
+    if ((add_xmlHttp.readyState == 0 || add_xmlHttp.readyState == 4)) {
         add_xmlHttp.open("GET", "../lib/addPerson.php?email=" + person, true);
         add_xmlHttp.onreadystatechange = Add_handleServerResponse;
         add_xmlHttp.send();
 
     }
 } 
+
+
+AddPerson('georgegkas@gmail.com');
